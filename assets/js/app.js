5f4241a23fe9459c851682f9f097fefe7796b71c
assyaref <syarif.revolusion@icloud.com>
2026-06-15 14:26:36 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,46 @@
+// Request API
+async function request(body) {
 
+    const response = await fetch(CONFIG.API.URL, {
+
+        method: "POST",
+
+        headers: {
+
+            "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify(body)
+
+    });
+
+    return await response.json();
+
+}
+// Session
+function setSession(user) {
+
+    localStorage.setItem(
+
+        CONFIG.STORAGE.SESSION,
+
+        JSON.stringify(user)
+
+    );
+
+}
+
+function getSession() {
+
+    return JSON.parse(
+
+        localStorage.getItem(
+
+            CONFIG.STORAGE.SESSION
+
+        )
+
+    );
+
+}

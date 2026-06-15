6958b5fce54b661932aa8c8926efd0c3efb0e11d
assyaref <syarif.revolusion@icloud.com>
2026-06-15 14:25:41 +0700

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,41 @@
+// Login
+async function login(email, password) {
 
+    const response = await request({
+
+        action: "login",
+
+        data: {
+
+            email,
+
+            password
+
+        }
+
+    });
+
+    if (!response.success) {
+
+        throw new Error(response.message);
+
+    }
+
+    setSession(response.data);
+
+    window.location.href = "dashboard.html";
+
+}
+
+// Logout
+function logout() {
+
+    localStorage.removeItem(CONFIG.STORAGE.SESSION);
+
+    localStorage.removeItem(CONFIG.STORAGE.USER);
+
+    window.location.replace("login.html");
+
+}
+// Check Session
+Check Session

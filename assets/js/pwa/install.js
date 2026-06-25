7d217954c80c3857daa1722af793f75add397cdd
assyaref <syarif.revolusion@icloud.com>
2026-06-25 12:24:46 +0700

    Update install.js

--- assets/js/pwa/install.js
+++ assets/js/pwa/install.js
@@ -1,1 +1,35 @@
+// ==========================================
+// INSTALL
+// ==========================================
 
+self.addEventListener(
+
+    "install",
+
+    event=>{
+
+        event.waitUntil(
+
+            caches.open(
+
+                STATIC_CACHE
+
+            )
+
+            .then(cache=>{
+
+                return cache.addAll(
+
+                    APP_SHELL
+
+                );
+
+            })
+
+        );
+
+        self.skipWaiting();
+
+    }
+
+);

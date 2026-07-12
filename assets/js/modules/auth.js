79de7ea7f418d8c0a3938ad8b60c77764d48e741
assyaref <syarif.revolusion@icloud.com>
2026-07-12 21:50:58 +0700

    Update auth.js

--- assets/js/modules/auth.js
+++ assets/js/modules/auth.js
@@ -106,11 +106,11 @@
 
             const role = sessionData.role || "USER";
             const routeMap = {
-                "USER": "user-report",
-                "GENERAL AFFAIR": "user-report",
-                "TECHNICIAN": "workorder,
-                "ADMIN": "dashboard",
-                "ADMINISTRATOR": "dashboard"
+                "USER": "user-report.html",
+                "GENERAL AFFAIR": "user-report.html",
+                "TECHNICIAN": "wo.html,
+                "ADMIN": "dashboard.html",
+                "ADMINISTRATOR": "dashboard.html"
             };
             const targetPage = routeMap[role.toUpperCase()] || "user-report.html";
 
